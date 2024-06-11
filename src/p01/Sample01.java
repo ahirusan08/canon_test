c5bead1bfb62b23f0f08808d112c5125177d08f7
student <student@WIN-GFJMOVS5CQH>
2024-06-11 14:04:25 +0900

    TeamB

---
+++ src/p01/Sample01.java
@@ -0,0 +1,12 @@
+package p01;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("小川 実咲");
+		System.out.println(22);
+	}
+
+}

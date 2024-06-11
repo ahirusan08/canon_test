971053237b077cd3846faae36670bf697f26cd64
student <student@WIN-QA12IFLCJ0P>
2024-06-11 14:29:43 +0900

    kikusui

---
+++ src/p01/KikusuiX.java
@@ -0,0 +1,17 @@
+package p01;
+
+public class KikusuiX {
+
+	public static void main(String[] args) {
+
+		int x = 22;
+		
+		if (x > 20) {
+			
+		System.out.println("菊水達彦です。");
+		System.out.println(x);
+		
+		}
+	}
+
+}

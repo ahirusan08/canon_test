7f307764db4aa4266093e28686dcb0debcf6d2b7
student <student@WIN-817G5PLIKEO>
2024-06-11 14:58:25 +0900

    love

--- src/taochan/tao1.java
+++ src/taochan/tao1.java
@@ -1,9 +1,35 @@
 package taochan;
 
+import java.util.Scanner;
+
 public class tao1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+
+		System.out.println("愛ください");
+
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("書いてくれたのは？:");
+		String name = scan1.next();
+
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("message1");
+		String ai1 = scan2.next();
+
+		Scanner scan3 = new Scanner(System.in);
+		System.out.println("message2");
+		String ai2 = scan3.next();
+
+		System.out.println("---");
+
+		System.out.println("こんにちは" + name + "です。");
+
+		String[] ary = { ai1, ai2 };
+		for (String data : ary) {
+			System.out.println("・" + data);
+		}
+		System.out.println("thankyou by tao");
 
 	}
 
